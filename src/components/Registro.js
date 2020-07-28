9a43e1a2b7601b1ab02658df7856285f53b04c4d
dederico <dederico@gmail.com>
2020-07-28 18:58:50 -0500

    Registro de nuevas familias candidatas

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -103,31 +103,51 @@ const Registro = () => {
           ...candidato,
           nombreEl: event.target.value,
         });
+        break;
+      case "nombreElla":
+        setCandidato({
+          ...candidato,
+          nombreElla: event.target.value,
+        });
+        break;
+      case "FechaNacimientoEl":
+        setCandidato({
+          ...candidato,
+          FechaNacimientoEl: event.target.value,
+        });
+        break;
+      case "FechaNacimientoElla":
+        setCandidato({
+          ...candidato,
+          FechaNacimientoElla: event.target.value,
+        });
+        break;
     }
-    const createCandidato = () => {
-      if (candidato.nombreEl.length > 0) {
-        axios
-          .post(
-            "https://adopciones-db12b.firebaseio.com/candidatos/-MDKwO6WiKmX9tvVW9mW/candidatos.json",
-            candidato
-          )
-          .then(({ data }) => {
-            if (data.nombreEl) {
-              history.push(`/familia/${data.nombreEl}`);
-            } else {
-              alert("Algo salio mal");
-            }
-          })
-          .catch(() => {
-            // si sigues desarrollando el proyecto, si está en tus manos repararlo
-            alert("Hay algo mal fuera de tus manos.");
-          });
-      } else {
-        alert(
-          "No has llenado todos los campos o has llenado de forma incorrecta un campo."
-        );
-      }
-    };
+  };
+
+  const createCandidato = () => {
+    if (candidato.nombreEl.length > 0) {
+      axios
+        .post(
+          "https://adopciones-db12b.firebaseio.com/candidatos/-MDKwO6WiKmX9tvVW9mW/candidatos.json",
+          candidato
+        )
+        .then(({ data }) => {
+          if (data.nombreEl) {
+            history.push(`/familia/${data.nombreEl}`);
+          } else {
+            alert("Algo salio mal");
+          }
+        })
+        .catch(() => {
+          // si sigues desarrollando el proyecto, si está en tus manos repararlo
+          alert("Hay algo mal fuera de tus manos.");
+        });
+    } else {
+      alert(
+        "No has llenado todos los campos o has llenado de forma incorrecta un campo."
+      );
+    }
   };
 
   return (
@@ -138,11 +158,47 @@ const Registro = () => {
       <input
         type="text"
         className="form-control"
-        id="apellidos"
+        id="NombrePadre"
+        placeholder="Nombre Completo del Padre"
         value={candidato.nombreEl}
         onChange={handleChange}
       />
+      <label htmlFor="title">Nombre </label>
+      <input
+        type="text"
+        className="form-control"
+        id="NombreMadre"
+        placeholder="Nombre Completo de la Madre"
+        value={candidato.nombreElla}
+        onChange={handleChange}
+      />
+      <label htmlFor="title">Fecha de Nacimiento Padre </label>
+      <input
+        type="date"
+        className="form-control"
+        id="NacimientoPadre"
+        placeholder="DD/MM/AA"
+        value={candidato.FechaNacimientoEl}
+        onChange={handleChange}
+      />
+      <label htmlFor="title">Fecha de Nacimiento Madre </label>
+      <input
+        type="date"
+        className="form-control"
+        id="NacimientoMadre"
+        placeholder="DD/MM/AA"
+        value={candidato.FechaNacimientoElla}
+        onChange={handleChange}
+      />
+      <button
+        type="submit"
+        className="btn btn-primary"
+        onClick={createCandidato}
+      >
+        Completar Registro
+      </button>
     </>
   );
 };
+
 export default Registro;
