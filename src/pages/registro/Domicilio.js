65db0c1b540cd0ff6b086ea6db33dde293482421
dederico <dederico@gmail.com>
2020-08-21 09:00:20 -0500

    commit x

--- src/pages/registro/Domicilio.js
+++ src/pages/registro/Domicilio.js
@@ -1,7 +1,181 @@
-import React from "react";
+import React, { useState } from "react";
+// import { useHistory } from "react-router-dom";
+import axios from "axios";
+import NavbarRegistro from "../NavbarRegistro";
+import "../../components/registro/Registro.css";
 
 const Domicilio = () => {
-  return console.log("yes");
+  // const history = useHistory();
+  const [candidato, setCandidato] = useState({
+    esSoltero: false,
+  });
+
+  const handleChange = (event) => {
+    // task { title, descript...}
+    // eslint-disable-next-line default-case
+    switch (event.target.id) {
+      case "Calle":
+        setCandidato({
+          ...candidato,
+          calle: event.target.value,
+        });
+        break;
+      case "colonia":
+        setCandidato({
+          ...candidato,
+          colonia: event.target.value,
+        });
+        break;
+      case "estado":
+        setCandidato({
+          ...candidato,
+          estado: event.target.value,
+        });
+        break;
+      case "telefono":
+        setCandidato({
+          ...candidato,
+          telefono: event.target.value,
+        });
+        break;
+      case "numero":
+        setCandidato({
+          ...candidato,
+          numero: event.target.value,
+        });
+        break;
+      case "municipio":
+        setCandidato({
+          ...candidato,
+          municipio: event.target.value,
+        });
+        break;
+      case "codigoPostal":
+        setCandidato({
+          ...candidato,
+          codigoPostal: event.target.value,
+        });
+        break;
+      default:
+        alert("Algo no está funcionando :c");
+        break;
+    }
+  };
+
+  const createCandidato = () => {
+    if (candidato.nombre.length > 0) {
+      axios
+        .post("https://adopciones-db12b-3ba3f.firebaseio.com/familias.json", {
+          clave: candidato.nombre,
+          fechaDeCreacion: new Date(),
+        })
+        .then(({ data }) => {
+          if (data.name) {
+            // data.name es el nuevo id
+            axios
+              .post(
+                `https://adopciones-db12b-3ba3f.firebaseio.com/familias/${data.name}/interesados.json`,
+                candidato
+              )
+              .then(({ data }) => alert(data));
+            // history.push(`/familia/${data.nombreEl}`);
+          } else {
+            alert("Algo salio mal en la peticón de crear familiar");
+          }
+        })
+        .catch(() => {
+          // si sigues desarrollando el proyecto, si está en tus manos repararlo
+          alert("Hay algo mal fuera de tus manos.");
+        });
+    } else {
+      alert(
+        "No has llenado todos los campos o has llenado de forma incorrecta un campo."
+      );
+    }
+  };
+
+  return (
+    <>
+      <NavbarRegistro />
+      <h1>Domicilio!</h1>
+
+      <label htmlFor="title">Calle</label>
+      <input
+        type="text"
+        className="form-control"
+        id="calleDomicilio"
+        placeholder="Nombre de la calle del domicilio"
+        value={candidato.calle}
+        onChange={handleChange}
+      />
+      <label htmlFor="title">Numero</label>
+      <input
+        type="number"
+        className="form-control"
+        id="numeroDomicilio"
+        placeholder="#300"
+        value={candidato.numero}
+        onChange={handleChange}
+      />
+      <label htmlFor="title">Colonia</label>
+      <input
+        type="text"
+        className="form-control"
+        id="coloniaDomicilio"
+        placeholder="Colonia del Domicilio"
+        value={candidato.colonia}
+        onChange={handleChange}
+      />
+      <label htmlFor="title">Municipio</label>
+      <input
+        type="text"
+        className="form-control"
+        id="municipioDomicilio"
+        placeholder="Municipio donde se ubica el domicilio."
+        value={candidato.municipio}
+        onChange={handleChange}
+      />
+      <label htmlFor="title">Estado </label>
+      <input
+        type="text"
+        className="form-control"
+        id="estado"
+        placeholder="Estado donde se ubica el domicilio "
+        value={candidato.estado}
+        onChange={handleChange}
+      />
+      <label htmlFor="title">Codigo Postal </label>
+      <input
+        type="text"
+        className="form-control"
+        id="codigoPostal"
+        placeholder="C.P."
+        value={candidato.codigoPostal}
+        onChange={handleChange}
+      />
+      <label htmlFor="title">Telefono </label>
+      <input
+        type="tel"
+        className="form-control"
+        id="Telefono"
+        placeholder="Telefono"
+        value={candidato.telefono}
+        onChange={handleChange}
+      />
+
+      <br />
+      <button
+        type="submit"
+        className="btn btn-primary"
+        onClick={createCandidato}
+      >
+        Completar Registro
+      </button>
+      <aside>
+        <img src="../../assets/06-dif (1).png" alt="imagen" />
+      </aside>
+    </>
+  );
 };
 
 export default Domicilio;
